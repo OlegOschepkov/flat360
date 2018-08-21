6c3921c65b6d610d2906d1dd1eb5d539af1cf402
OlegOschepkov <36763883+OlegOschepkov@users.noreply.github.com>
2018-08-21 08:23:47 +0400

    Styles

--- gulpfile.js/tasks/ghpages.js
+++ gulpfile.js/tasks/ghpages.js
@@ -1,4 +1,10 @@
-const gulp = require('gulp');
-const ghpages = require('gulp-gh-pages');
+const gulp = require("gulp");
 
-module.exports = () => gulp.src('dist/**/*').pipe(ghpages());
+const ghpages = require("gulp-gh-pages");
+
+const options = {
+  remoteUrl: "https://olegoschepkov.github.io/",
+  branch: "master"
+};
+
+module.exports = () => gulp.src("dist/**/*").pipe(ghpages(options));
