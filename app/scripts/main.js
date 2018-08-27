c168ae6245e39ca36252ceb5a458ec569f182127
OlegOschepkov <36763883+OlegOschepkov@users.noreply.github.com>
2018-08-27 16:51:26 +0400

    v0.95

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 (function() {
-  "use strict";
+  ("use strict");
 
   const btn = document.querySelector(".js-toggle_header-menu");
   const headerMenu = document.querySelector(".header-menu");
@@ -9,9 +9,22 @@
   btn.addEventListener("click", function() {
     if (headerMenu.classList.contains("visually-hidden")) {
       headerMenu.classList.remove("visually-hidden");
+      btn.classList.add("is-active");
+      btn.classList.remove("unfreeze");
+      btn.classList.add("freeze");
     } else if (!headerMenu.classList.contains("visually-hidden")) {
       headerMenu.classList.add("visually-hidden");
+      btn.classList.remove("is-active");
+      btn.classList.remove("freeze");
+      btn.classList.add("unfreeze");
     }
+  });
+
+  $(document).on("click", ".js-dropdown", function() {
+    $(this).toggleClass("is-active");
+    $(this)
+      .find(".js-dropdown-list")
+      .slideToggle();
   });
 })();
 
