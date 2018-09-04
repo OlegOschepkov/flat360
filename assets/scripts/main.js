2e3d0dbda7cb99e1f2bb4c140e241159378f0f83
OlegOschepkov <36763883+OlegOschepkov@users.noreply.github.com>
2018-09-04 13:49:38 +0400

    Update 2018-09-04T09:49:31.643Z

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,6 +5,26 @@
 
   const btn = document.querySelector(".js-toggle_header-menu");
   const headerMenu = document.querySelector(".header-menu");
+  const jsSlider1 = document.querySelectorAll(".js-slider1");
+  const jsSlider2 = document.querySelectorAll(".js-slider2");
+  const jsSlider1Arr = Array.from(jsSlider1);
+  const jsSlider2Arr = Array.from(jsSlider2);
+  const elementTexts = document.querySelectorAll(".advantages-list__item-text");
+
+  function slider(jsSliderArr) {
+    jsSliderArr[0].classList.add("active");
+    jsSliderArr.forEach(function(element) {
+      element.addEventListener("click", function() {
+        jsSliderArr.forEach(function(elem) {
+          elem.classList.remove("active");
+        });
+        element.classList.add("active");
+      });
+    });
+  }
+
+  slider(jsSlider1Arr);
+  slider(jsSlider2Arr);
 
   btn.addEventListener("click", function() {
     if (headerMenu.classList.contains("visually-hidden")) {
